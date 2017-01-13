a26307c7aedb51a8f3f019209e7e5b9f8ba0001e
vitaly-emelianov <vitalik.emelianov@gmail.com>
2017-01-13 16:39:44 +0300

    Fix.

--- anonimization.py
+++ anonimization.py
@@ -9,11 +9,11 @@ for branch_id in data.BRANCH_ID.unique():
     fake_ids[branch_id] = counter * 1000
     counter += 1
 
-# Anonimising branch ids
+# Anonimizing branch ids
 data['FAKE_ID'] = data["BRANCH_ID"].apply(lambda x: fake_ids[x])
 data = data.drop(axis=1, labels=["BRANCH_ID", ])
 data = data.rename(index=str, columns={"FAKE_ID": "BRANCH_ID", })
 
-# Anonimising number of clients registered
+# Anonimizing number of clients registered
 data["CLIENTS_REGGED"] = data["CLIENTS_REGGED"].apply(lambda x: x * 3)
 data.to_csv("data.csv", index=False)
